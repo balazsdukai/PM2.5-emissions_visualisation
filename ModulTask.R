ae0c5ae261fa2a69e9675c990e47f793b36acaf2
Balazs Dukai <balazs.dukai@gmail.com>
2015-04-23 17:31:01 +0200

    Histogram implemented

--- ModulTask.R
+++ ModulTask.R
@@ -1,6 +1,8 @@
 library(ggplot2)
 source("HM_year.R")
 library (lmomco)
+library (maptools)t
+library (maps)
 
 ## This first line will likely take a few seconds. Be patient!
 NEI <- readRDS("summarySCC_PM25.rds")
@@ -23,6 +25,25 @@ p  <- ggplot(NEI_SCC1_50, aes(x=year, y=Emissions)) + geom_point() +
     geom_point(data=NEI_1_50_HM, aes(x=year, y=HM), shape=1, colour="Red") + 
     geom_text(data=NEI_1_50_HM, aes(x=year, y=HM, label=round(NEI_1_50_HM$HM, digits=3)), 
               hjust=-0.3, vjust=-0.4, size=4, colour="Red")
+# fuck this is crap, the harmonic mean doesnt really work...
+
+# I need to see how the data is distributed and whats the case with the outliers.
+# For that, the best tools are the histogram, density plot, boxplot.
+# So lets check a histogram first
+# Use year as the faceting variable
+ggplot(NEI_SCC1_50, aes(x=Emissions)) + geom_histogram(fill="red", colour="grey", binwidth=50) +
+    facet_grid(year ~ .)
 
 
+# boxplots
+qplot(year,Emissions, data=NEI_SCC1_50, geom="boxplot")
+attach(NEI_SCC1_50)
+boxplot(Emissions,  data=NEI_SCC1_50)
 
+
+abc <- NEI_SCC1_50$Emissions
+b <- ggplot(abc, aes(x = X1, ymin = `0%`, lower = `25%`,
+                     middle = `50%`, upper = `75%`, ymax = `100%`))
+b + geom_boxplot(stat = "identity")
+b + geom_boxplot(stat = "identity") + coord_flip()
+b + geom_boxplot(aes(fill = X1), stat = "identity")
